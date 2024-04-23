c9cb9fdf397c64b5bb06347e2c85c4d2770440eb
ja99k <jannikn112@gmail.com>
2024-04-23 20:07:40 +0200

    add human choice

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello!")
+function getComputerChoice() {
+  var choices = {
+    1: "Rock",
+    2: "Paper",
+    3: "Scissors",
+  };
+
+  var rand = Math.floor(Math.random() * Object.keys(choices).length) + 1;
+  return choices[rand].toLowerCase();
+}
+
+function getHumanChoice() {
+  var answer = prompt("Rock, Paper or Scissors?");
+  return answer.toLowerCase();
+}
+
+console.log(getHumanChoice());
+console.log(getComputerChoice());
